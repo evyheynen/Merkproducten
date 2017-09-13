36420646f036c8839eec064d091444603b8cdd65
Evy <evy.heynen@telenet.be>
2017-09-13 15:13:30 +0200

    modellen zijn toegevoegd

---
+++ src/be/oak3/model/Deodorant.java
@@ -0,0 +1,19 @@
+package be.oak3.model;
+
+public class Deodorant extends Product{
+
+    private enum DeoType{
+        VAPO, STICK;
+    }
+    private DeoType soort;
+
+    public Deodorant(int productNummer, String merk, String naam, int volume, double prijs, DeoType soort) {
+        super(productNummer, merk, naam, volume, prijs);
+        this.soort = soort;
+    }
+    @Override
+    public String toString{
+        return super.toString()+"\t"+soort;
+    }
+
+}
